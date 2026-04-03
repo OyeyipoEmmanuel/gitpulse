39a5747749f88f7cd3ece0bfa6544133a9ae13cc
Emmanuel <emmasco0207@gmail.com>
2026-04-03 03:15:36 +0100

    initial commit

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useAuthStore } from '../../store/authStore'
+
+const Dashboard = () => {
+    const {user, loading} = useAuthStore()
+    if(!loading){
+        console.log(user?.user_metadata)
+    }
+  return (
+    <div>dashboard</div>
+  )
+}
+
+export default Dashboard
