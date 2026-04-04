ccbb830be534e7c766eaea7e8c152f6983a3b398
Emmanuel <emmasco0207@gmail.com>
2026-04-04 21:33:27 +0100

    feat: select account and dashboards(in progress)

--- src/features/global/fetchAllAccount.tsx
+++ src/features/global/fetchAllAccount.tsx
@@ -3,26 +3,29 @@
 import { useQuery } from "@tanstack/react-query"
 import { useAuthStore } from "../../store/authStore"
 
-export const useGetAccountsToDisplay = ()=>{
-    const {session, loading} = useAuthStore();
+export const useGetAccountsToDisplay = () => {
+    const { session, loading } = useAuthStore();
 
     const token = session?.provider_token
 
-    const headers = {Authorization: `Bearer ${token}`}
+    const headers = { Authorization: `Bearer ${token}` }
 
     return useQuery({
         queryKey: ['fetch_all_accounts'],
         enabled: !loading && !!token,
-        queryFn: async()=>{
+        retry: 2,
+        staleTime: 1000 * 60 * 5, // 5 mins
+        gcTime: 1000 * 60 * 10,
+        queryFn: async () => {
             const [userRes, orgRes] = await Promise.all([
-                fetch("https://api.github.com/user", {headers}),
-                fetch("https://api.github.com/user/orgs", {headers}),
+                fetch("https://api.github.com/user", { headers }),
+                fetch("https://api.github.com/user/orgs", { headers }),
             ])
 
             const user = await userRes.json()
             const orgs = await orgRes.json()
 
-            return {user, orgs}
+            return { user, orgs }
         }
     })
 
