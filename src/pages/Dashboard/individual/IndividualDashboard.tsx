4dbadd7cd4b3f468ff7309370c08f489fe273711
Emmanuel <emmasco0207@gmail.com>
2026-04-04 21:33:29 +0100

    feat: select account and dashboards(in progress)

---
+++ src/pages/Dashboard/individual/IndividualDashboard.tsx
@@ -0,0 +1,12 @@
+
+import IndividualSideNav from './components/IndividualSideNav'
+
+const IndividualDashboard = () => {
+  return (
+    <main>
+      <IndividualSideNav/>
+    </main>
+  )
+}
+
+export default IndividualDashboard
