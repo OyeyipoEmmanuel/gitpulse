58fa5706d3ac13a2ed333117e5cc8ab6cd303344
Emmanuel <emmasco0207@gmail.com>
2026-04-04 21:33:28 +0100

    feat: select account and dashboards(in progress)

--- src/pages/auth/AuthCallback.tsx
+++ src/pages/auth/AuthCallback.tsx
@@ -1,18 +1,22 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuthStore } from '../../store/authStore'
+import { LoadingSpinner } from '@/components/ui/spinner'
 
 //Renders when supabase successfully signs in github
 const AuthCallback = () => {
     const navigate = useNavigate()
     const { user, loading } = useAuthStore()
 
-
-
-
     //Navigates to /dashboard when component mounts
     useEffect(() => {
-        if (loading) return; // wait until session is resolved
+        if (loading) {
+            <div className='w-screen h-screen bg-primaryBg'>
+                <LoadingSpinner className='text-green-500 w-32 h-32' />
+            </div>
+
+            return;
+        }; // wait until session is resolved
 
         if (user) {
             navigate(`/${user.user_metadata.user_name}/select-account`, { replace: true })
@@ -22,7 +26,10 @@ const AuthCallback = () => {
         }
     }, [user, loading])
     return (
-        <div>Redirecting....</div>
+        <div className='w-screen h-screen bg-primaryBg'>
+            <h1 className='text-4xl'>Redirection..</h1>
+            <LoadingSpinner className='bg-green-500 w-32 h-32' />
+        </div>
     )
 }
 
