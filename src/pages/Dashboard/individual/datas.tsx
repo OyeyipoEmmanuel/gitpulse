8b90329200333d9fc9e4439f81d28d10cf21cf45
Emmanuel <emmasco0207@gmail.com>
2026-04-06 03:48:30 +0100

    feat: Individual dashboard - side nav and select repo

--- src/pages/Dashboard/individual/datas.tsx
+++ src/pages/Dashboard/individual/datas.tsx
@@ -5,31 +5,31 @@ export const navContent: DashboardNavCOntentType[] = [
     {
       label: "Profile",
       path:"/profile",
-      icon: <UserRound color="#94A3B8" strokeWidth={1} />
+      icon: <UserRound size={20} color="#94A3B8" strokeWidth={1} />
     },
     {
       label: "Contributions",
       path:"/contributions",
-      icon: <ChartNoAxesColumn size={32} color="#94A3B8" strokeWidth={1.5} />
+      icon: <ChartNoAxesColumn size={20} color="#94A3B8" strokeWidth={1.5} />
     },
     {
       label: "Repos",
       path:"/repos",
-      icon: <FolderGit size={32} color="#94A3B8" strokeWidth={1} />
+      icon: <FolderGit size={20} color="#94A3B8" strokeWidth={1} />
     },
     {
       label: "PRs",
       path:"/prs",
-      icon: <GitBranch size={32} color="#94A3B8" strokeWidth={1} />
+      icon: <GitBranch size={20} color="#94A3B8" strokeWidth={1} />
     },
     {
       label: "Issues",
       path:"/issues",
-      icon: <Info size={32} color="#94A3B8" strokeWidth={1} />
+      icon: <Info size={20} color="#94A3B8" strokeWidth={1} />
     },
     {
       label: "Activity",
       path:"/activity",
-      icon: <ChartNoAxesCombined size={32} color="#94A3B8" strokeWidth={1} />
+      icon: <ChartNoAxesCombined size={20} color="#94A3B8" strokeWidth={1} />
     },
   ]
