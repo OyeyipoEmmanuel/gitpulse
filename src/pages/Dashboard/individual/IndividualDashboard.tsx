0a713654422c0184a3acb1a740b7b6bf069f052d
Emmanuel <emmasco0207@gmail.com>
2026-04-08 01:41:36 +0100

    feat: dashboards side nav, top nav and profile screen started

--- src/pages/Dashboard/individual/IndividualDashboard.tsx
+++ src/pages/Dashboard/individual/IndividualDashboard.tsx
@@ -1,10 +1,17 @@
 
+import DashboardTopNav from '@/components/navbars/DashboardTopNav'
 import IndividualSideNav from './components/IndividualSideNav'
 
 const IndividualDashboard = () => {
   return (
-    <main>
-      <IndividualSideNav/>
+    <main className='flex flex-row w-full '>
+      <section className='w-[256px]'>
+        <IndividualSideNav />
+      </section>
+
+      <section className='w-[calc(100%-256px)]'>
+        <DashboardTopNav/>
+      </section>
     </main>
   )
 }
