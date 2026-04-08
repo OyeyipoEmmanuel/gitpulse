e8088ff2b776700502ea39017e0f7a51006bf453
Emmanuel <emmasco0207@gmail.com>
2026-04-08 01:41:37 +0100

    feat: dashboards side nav, top nav and profile screen started

--- src/services/fetchAllAccount.tsx
+++ src/services/fetchAllAccount.tsx
@@ -4,24 +4,32 @@ import { useQuery } from "@tanstack/react-query"
 import { useAuthStore } from "../store/authStore"
 
 export const useGetAccountsToDisplay = () => {
-    const { session, loading } = useAuthStore();
+    const { loading, providerToken, getToken } = useAuthStore();
     const url = import.meta.env.VITE_GITHUB_API_URL
 
-    const token = session?.provider_token
-    console.log(session)
 
-    const headers = { Authorization: `Bearer ${token}` }
+
 
     return useQuery({
         queryKey: ['fetch_all_accounts'],
-        enabled: !loading && !!token,
+        enabled: !loading && !!providerToken,
         retry: 2,
         staleTime: 1000 * 60 * 5, // 5 mins
         gcTime: 1000 * 60 * 10,
         queryFn: async () => {
+            const token = await getToken()
+
             const [userRes, orgRes] = await Promise.all([
-                fetch(`${url}/user`, { headers }),
-                fetch(`${url}/user/orgs`, { headers }),
+                fetch(`${url}/user`,
+                    {
+                        headers: { Authorization: `Bearer ${token}` }
+                    }
+                ),
+                fetch(`${url}/user/orgs`,
+                    {
+                        headers: { Authorization: `Bearer ${token}` }
+                    }
+                ),
             ])
 
             if (!userRes.ok) throw new Error(userRes.statusText)
