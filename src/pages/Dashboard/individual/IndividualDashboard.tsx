b1b37e0e4744ed8b4aeec4594c637a3e244311fc
Emmanuel <emmasco0207@gmail.com>
2026-04-13 04:22:24 +0100

    feat: Individual dashboard profile page. added profile card and language distribution chart

--- src/pages/Dashboard/individual/IndividualDashboard.tsx
+++ src/pages/Dashboard/individual/IndividualDashboard.tsx
@@ -1,16 +1,23 @@
 
 import DashboardTopNav from '@/components/navbars/DashboardTopNav'
 import IndividualSideNav from './components/IndividualSideNav'
+import IndividualProfile from './pages/IndividualProfile'
 
 const IndividualDashboard = () => {
   return (
     <main className='flex flex-row w-full '>
-      <section className='w-[256px]'>
+      <section className='hidden md:block w-[256px]'>
         <IndividualSideNav />
       </section>
 
-      <section className='w-[calc(100%-256px)]'>
-        <DashboardTopNav/>
+      <section className='md:w-[calc(100%-256px)] w-full'>
+        <DashboardTopNav />
+
+        {/* Pages */}
+        <div className='md:px-4'>
+          <IndividualProfile />
+        </div>
+
       </section>
     </main>
   )
