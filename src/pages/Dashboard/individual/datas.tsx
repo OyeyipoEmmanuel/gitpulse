1d84d70668e0d278fb7de87a1e1df0b9a08477fd
Emmanuel <emmasco0207@gmail.com>
2026-04-17 23:13:29 +0100

    feat: changing nav menus, replacing screens with better contend - started repo intelligence screen

--- src/pages/Dashboard/individual/datas.tsx
+++ src/pages/Dashboard/individual/datas.tsx
@@ -1,35 +1,40 @@
 import type { DashboardNavCOntentType } from "@/types";
-import { ChartNoAxesColumn, ChartNoAxesCombined, FolderGit, GitBranch, Info, UserRound } from "lucide-react";
+import { UserRound } from "lucide-react";
+import { FaCode } from "react-icons/fa6";
+import { PiGauge } from "react-icons/pi";
+import { IoIosPeople } from "react-icons/io";
+import { PiTrendUp } from "react-icons/pi";
+import { MdWorkOutline } from "react-icons/md";
 
 export const navContent: DashboardNavCOntentType[] = [
-    {
-      label: "Profile",
-      path:"profile",
-      icon: <UserRound size={20} color="#94A3B8" strokeWidth={1} />
-    },
-    {
-      label: "Contributions",
-      path:"contributions",
-      icon: <ChartNoAxesColumn size={20} color="#94A3B8" strokeWidth={1.5} />
-    },
-    {
-      label: "Repos",
-      path:"repos",
-      icon: <FolderGit size={20} color="#94A3B8" strokeWidth={1} />
-    },
-    {
-      label: "PRs",
-      path:"prs",
-      icon: <GitBranch size={20} color="#94A3B8" strokeWidth={1} />
-    },
-    {
-      label: "Issues",
-      path:"issues",
-      icon: <Info size={20} color="#94A3B8" strokeWidth={1} />
-    },
-    {
-      label: "Activity",
-      path:"activity",
-      icon: <ChartNoAxesCombined size={20} color="#94A3B8" strokeWidth={1} />
-    },
-  ]
+  {
+    label: "Profile",
+    path: "profile",
+    icon: <UserRound size={20} color="#94A3B8" strokeWidth={1} />
+  },
+  {
+    label: "Repo Intelligence",
+    path: "repo-intelligence",
+    icon: <FaCode size={20} color="#94A3B8"  />
+  },
+  {
+    label: "Productivity",
+    path: "productivity",
+    icon: <PiGauge size={20} color="#94A3B8" />
+  },
+  {
+    label: "Collaborations",
+    path: "collaborations",
+    icon: <IoIosPeople size={20} color="#94A3B8" />
+  },
+  {
+    label: "Impact Summary",
+    path: "impact-summary",
+    icon: <PiTrendUp size={20} color="#94A3B8" />
+  },
+  {
+    label: "Career Snapshot",
+    path: "career-snapshot",
+    icon: <MdWorkOutline size={20} color="#94A3B8" />
+  },
+]
