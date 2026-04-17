0ca63f965225fd6f0f4d0e4e3b2cc994ef0ba06d
Emmanuel <emmasco0207@gmail.com>
2026-04-17 23:13:31 +0100

    feat: changing nav menus, replacing screens with better contend - started repo intelligence screen

--- src/pages/Dashboard/organization/OrganizationDashboard.tsx
+++ src/pages/Dashboard/organization/OrganizationDashboard.tsx
@@ -1,10 +1,24 @@
+import DashboardTopNav from "@/components/navbars/DashboardTopNav"
 import OrgSidenav from "./components/OrgSidenav"
+import OrgProfile from "./pages/OrgProfile"
 
 
 const OrganizationDashboard = () => {
   return (
-    <main>
-      <OrgSidenav/>
+    <main className='flex flex-row w-full '>
+      <section className='hidden md:block w-[256px]'>
+        <OrgSidenav />
+      </section>
+
+      <section className='md:w-[calc(100%-256px)] w-full'>
+        <DashboardTopNav />
+
+        {/* Pages */}
+        <div className='md:px-4'>
+          <OrgProfile />
+        </div>
+
+      </section>
     </main>
   )
 }
