fb8f5ec256d613eb3664f6e0443c81dbf8784d2d
Emmanuel <emmasco0207@gmail.com>
2026-04-20 00:27:09 +0100

    feat: repo intelligence page

--- src/pages/Dashboard/individual/IndividualDashboard.tsx
+++ src/pages/Dashboard/individual/IndividualDashboard.tsx
@@ -1,17 +1,24 @@
 
+import { useState } from 'react'
 import DashboardTopNav from '@/components/navbars/DashboardTopNav'
 import IndividualSideNav from './components/IndividualSideNav'
 import { Outlet } from 'react-router-dom'
 
 const IndividualDashboard = () => {
+  const [mobileNavOpen, setMobileNavOpen] = useState(false)
+
   return (
-    <main className='flex flex-row w-full '>
-      <section className='hidden md:block w-[256px]'>
-        <IndividualSideNav />
-      </section>
+    <main className='flex flex-row w-full'>
+      {/* Spacer for fixed desktop sidebar */}
+      <div className='hidden md:block w-[256px] shrink-0' />
+
+      <IndividualSideNav
+        isOpen={mobileNavOpen}
+        onClose={() => setMobileNavOpen(false)}
+      />
 
       <section className='md:w-[calc(100%-256px)] w-full'>
-        <DashboardTopNav />
+        <DashboardTopNav onMenuToggle={() => setMobileNavOpen(true)} />
 
         {/* Pages */}
         <div className='md:px-4'>
