c63e13a5f553a3dc9f81285e042f672c7ad118f5
Emmanuel <emmasco0207@gmail.com>
2026-04-25 02:17:07 +0100

    feat: productivity page done. added types for vars that had any. fixed responsiveness issues

--- src/components/charts/index.ts
+++ src/components/charts/index.ts
@@ -1,2 +1,4 @@
 export { default as LanguageDistributionChart } from "./LanguageDistributionChart";
 export { default as PrStatusPieChart } from "./PrStatusPieChart";
+export { default as MostProductiveDaysChart } from "./MostProductiveDaysChart";
+export { default as HourlyPerformanceChart } from "./HourlyPerformanceChart";
