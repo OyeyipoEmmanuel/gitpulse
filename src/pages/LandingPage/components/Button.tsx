82719950332998b90305d5b22a092a8cf45e4864
Emmanuel <emmasco0207@gmail.com>
2026-04-25 02:17:13 +0100

    feat: productivity page done. added types for vars that had any. fixed responsiveness issues

--- src/pages/LandingPage/components/Button.tsx
+++ src/pages/LandingPage/components/Button.tsx
@@ -1,4 +1,6 @@
-const Button = ({ icon, label, bgColor, textColor }: any) => {
+import type { ButtonProps } from "@/types"
+
+const Button = ({ icon, label, bgColor, textColor }: ButtonProps) => {
   return (
     <button
       className={`flex space-x-3 w-full text-xl py-4 items-center justify-center rounded-lg cursor-pointer hover:opacity-70 duration-300 transition-all`}
