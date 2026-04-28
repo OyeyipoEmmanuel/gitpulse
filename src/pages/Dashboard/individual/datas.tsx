f30c7898aed7a4c48c0cf0a84f493bf3401ef7c8
Emmanuel <emmasco0207@gmail.com>
2026-04-28 21:44:45 +0100

    chore:Individual report card screen. datas fetched. ui left

--- src/pages/Dashboard/individual/datas.tsx
+++ src/pages/Dashboard/individual/datas.tsx
@@ -2,9 +2,10 @@ import type { DashboardNavCOntentType } from "@/types";
 import { UserRound } from "lucide-react";
 import { FaCode } from "react-icons/fa6";
 import { PiGauge } from "react-icons/pi";
-import { IoIosPeople } from "react-icons/io";
 import { PiTrendUp } from "react-icons/pi";
 import { MdWorkOutline } from "react-icons/md";
+import { TbReportAnalytics } from "react-icons/tb";
+
 
 export const navContent: DashboardNavCOntentType[] = [
   {
@@ -23,9 +24,9 @@ export const navContent: DashboardNavCOntentType[] = [
     icon: <PiGauge size={20} color="#94A3B8" />
   },
   {
-    label: "Collaborations",
-    path: "collaborations",
-    icon: <IoIosPeople size={20} color="#94A3B8" />
+    label: "Report Card",
+    path: "dev-report-card",
+    icon: <TbReportAnalytics size={20} color="#94A3B8" />
   },
   {
     label: "Impact Summary",
