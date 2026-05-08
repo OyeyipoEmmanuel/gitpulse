67715e4671ea84cca37353d6c98d1032c641c400
Emmanuel <emmasco0207@gmail.com>
2026-05-08 01:58:56 +0100

    chore:careers snapshot page

--- src/pages/Dashboard/individual/pages/IndividualImpactSummary.tsx
+++
@@ -1,9 +0,0 @@
-const IndividualImpactSummary = () => {
-  return (
-    <main className="py-4 px-4 md:px-0 flex flex-col gap-6">
-      
-    </main>
-  )
-}
-
-export default IndividualImpactSummary
