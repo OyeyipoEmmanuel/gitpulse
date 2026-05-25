85f7ef7f7a19feed35a0951224b854d447865825
Emmanuel <emmasco0207@gmail.com>
2026-05-25 21:53:54 +0100

    chore: career snapshot page

--- src/pages/Dashboard/individual/pages/IndividualCareerSnapshot.tsx
+++ src/pages/Dashboard/individual/pages/IndividualCareerSnapshot.tsx
@@ -10,6 +10,9 @@ import { openSourceFunc } from "./IndividualReportCard"
 import Card from "../components/Card"
 import { getTimeAgo } from "@/lib/timeAgo"
 import { GrLineChart } from "react-icons/gr";
+import { IoStarSharp } from "react-icons/io5";
+import { MdOutlineFileDownload } from "react-icons/md";
+
 
 
 
@@ -68,11 +71,13 @@ async function getAllTimeContributionCount(
         token
     )
 
+    console.log(data)
+
     return data
 }
 
 function getLanguageMastered(repoNodes: RepositoryNode[]) {
-    
+
     const map = new Map<string, { count: number; color: string }>()
     const res: { name: string; color: string }[] = []
 
@@ -129,9 +134,6 @@ const IndividualCareerSnapshot = () => {
     if (error) return <ErrorToast message={error.message} />
     if (isPending || loading) return <LoadingSpinner className="text-green-500 w-32 h-32" />
 
-    console.log(data)
-
-    console.log(totalContributionObject?.user)
 
     const githubAge = getTimeAgo(data?.userGithubAge)
     console.log(githubAge)
@@ -142,23 +144,42 @@ const IndividualCareerSnapshot = () => {
         0
     )
 
+    //Get busiest year
+    const busiestYear = Object.entries(totalContributionObject ? totalContributionObject?.user : {}).reduce((max: { year: string; count: number }, [year, yearData]: [string, any]) =>
+        yearData.contributionCalendar.totalContributions > max.count ? { year, count: yearData.contributionCalendar.totalContributions }
+            : max
+        , { year: "", count: 0 })
+
+    const currentYear = new Date().getFullYear()
+    const busiestYearNum = parseInt(busiestYear.year.replace("y", ""))
+
+    const daysInBusiestYear = busiestYearNum === currentYear
+        ? Math.floor((new Date().getTime() - new Date(`${currentYear}-01-01`).getTime()) / (1000 * 60 * 60 * 24))
+        : 365
+
     //OS contribution count
     const OSContributionCount = openSourceFunc(data?.reportCard?.openSource?.user?.pullRequests?.nodes, params.username!).stats["PRs Merged"]
 
     const langMastered = getLanguageMastered(data?.reportCard?.repoData?.overview?.user?.repositories?.nodes)
 
-    console.log(data?.reportCard?.repoData)
 
     return (
         <main className="fullPageGradientBg min-h-screen">
             <section className="py-4 px-4 flex flex-col gap-6">
-                <p className="text-graySubtextColor">A high-fidelity analysis of your engineering journey on Github.</p>
+                <div className="flex flex-col justify-between md:flex-row">
+                    <p className="text-graySubtextColor">A high-fidelity analysis of your engineering journey on Github.</p>
+                    <button className="w-fit self-end tracking-tight bg-[#248637] rounded-xs text-center px-6 py-2 md:self-start text-white text-sm font-semibold cursor-pointer hover:opacity-70 transition-all duration-200 flex items-center gap-2">
+                        <MdOutlineFileDownload />
+                        <p>Download as PNG</p>
+                    </button>
+                </div>
+
 
                 {/* Row 1 */}
                 <section className="">
                     <Card className="w-full grid grid-cols-1 md:grid-cols-3 ">
                         <div className="p-5 md:border-b-none md:border-r border-b border-[#2A2F36] flex flex-col gap-3">
-                            <p className="text-graySubtextColor uppercase text-[10px] font-semibold tracking-wider">github age</p>
+                            <p className="text-graySubtextColor uppercase text-[12px] font-bold tracking-wider">github age</p>
                             <span className="flex text-white numbersFont">
                                 {/* Years */}
                                 <p className="text-5xl font-extrabold">{githubAge.years > 0 && githubAge.years}</p>
@@ -170,14 +191,14 @@ const IndividualCareerSnapshot = () => {
                             </span>
                         </div>
                         <div className="p-5 md:border-b-none md:border-r border-b border-[#2A2F36] flex flex-col gap-3">
-                            <p className="text-graySubtextColor uppercase text-[10px] font-semibold tracking-wider">all-time contributions</p>
+                            <p className="text-graySubtextColor uppercase text-[12px] font-bold tracking-wider">all-time contributions</p>
                             <span className="flex gap-2">
                                 <GrLineChart className="text-secondaryTextColor self-center" />
                                 <p className="text-secondaryTextColor tracking-wider text-5xl numbersFont">{totalContributionCount ?? "..."}</p>
                             </span>
                         </div>
                         <div className="p-5 flex flex-col gap-3">
-                            <p className="text-graySubtextColor uppercase text-[10px] font-semibold tracking-wider">os contribution count</p>
+                            <p className="text-graySubtextColor uppercase text-[12px] font-bold tracking-wider">os contribution count</p>
 
                             <span className="flex gap-1">
                                 <p className="text-5xl font-extrabold text-white">{OSContributionCount}</p>
@@ -195,7 +216,7 @@ const IndividualCareerSnapshot = () => {
                         <p className="text-graySubtextColor text-xs pt-1 font-light">Languages used in 3 and more repos.</p>
 
                         <div className="grid grid-cols-2 gap-3 pt-12">
-                            {langMastered.map((lang, idx) =>(
+                            {langMastered.map((lang, idx) => (
                                 <span key={idx} className="bg-[#21262D] border border-[#2A3037] rounded-full px-4 py-1 flex gap-3 items-center text-white ">
                                     <div style={{
                                         backgroundColor: lang.color
@@ -205,6 +226,20 @@ const IndividualCareerSnapshot = () => {
                             ))}
                         </div>
                     </Card>
+
+                    <div className="bg-[#0F1D1B] border border-[#133120] rounded-lg p-5 flex flex-col justify-between gap-6">
+                        <span className="flex justify-between">
+                            <p className="text-secondaryTextColor uppercase text-[12px] font-bold tracking-widest">busiest year ever</p>
+                            <IoStarSharp className="text-3xl text-[#238636]" />
+                        </span>
+                        <span>
+                            <p className="text-white tracking-wider text-5xl numbersFont">{busiestYear.year.slice(1,)}</p>
+                            <p className="text-secondaryTextColor text-2xl numbersFont">{busiestYear.count} contributions</p>
+                        </span>
+                        <p className="flex gap-1 text-graySubtextColor text-sm">
+                            Averaging <p className="font-semibold">{(busiestYear.count / daysInBusiestYear).toFixed(1)} </p> activities per day during peak cycles.
+                        </p>
+                    </div>
                 </section>
 
                 {/* Row 3 */}
