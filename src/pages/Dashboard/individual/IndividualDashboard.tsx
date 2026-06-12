283f5b854c8e673affcacabcc15718d02e297d07
Emmanuel <emmasco0207@gmail.com>
2026-06-12 08:57:37 +0100

    chore: Individual Career snapshot page

--- src/pages/Dashboard/individual/IndividualDashboard.tsx
+++ src/pages/Dashboard/individual/IndividualDashboard.tsx
@@ -1,11 +1,28 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import DashboardTopNav from '@/components/navbars/DashboardTopNav'
 import IndividualSideNav from './components/IndividualSideNav'
 import { Outlet } from 'react-router-dom'
+import { useAuthStore } from '@/store/authStore'
+import { useRepos } from '@/services/useRepos'
 
 const IndividualDashboard = () => {
   const [mobileNavOpen, setMobileNavOpen] = useState(false)
+  const [token, setToken] = useState<string | null>(null)
+  const params = useParams()
+  const { getToken, loading } = useAuthStore()
+  const username = params.username ?? ''
+
+  useEffect(() => {
+    if (!loading) {
+      getToken().then((value) => {
+        setToken(value)
+      })
+    }
+  }, [getToken, loading])
+
+  const { hasNextPage, hasPreviousPage, goToNextPage, goToPreviousPage } = useRepos(username, token)
 
   return (
     <main className='flex flex-row w-full'>
@@ -18,7 +35,13 @@ const IndividualDashboard = () => {
       />
 
       <section className='md:w-[calc(100%-256px)] w-full'>
-        <DashboardTopNav onMenuToggle={() => setMobileNavOpen(prev => !prev)} />
+        <DashboardTopNav
+          onMenuToggle={() => setMobileNavOpen((prev) => !prev)}
+          onPrevPage={goToPreviousPage}
+          onNextPage={goToNextPage}
+          hasPreviousPage={hasPreviousPage}
+          hasNextPage={hasNextPage}
+        />
 
         {/* Pages */}
         <div className=''>
