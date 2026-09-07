84ef60f7320632b5d368f0a5312e936b1d72efd1
Emmanuel <emmasco0207@gmail.com>
2026-09-07 08:30:35 +0100

    added tests, made major auth changes, few integration fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/queryClient'
 
-const client = new QueryClient()
 createRoot(document.getElementById('root')!).render(
 
   <BrowserRouter>
     <StrictMode>
-      <QueryClientProvider client={client}>
+      <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </StrictMode>
