b132c8d0e934ce5410bf2fbf25cbc42c6e6bb627
Emmanuel <emmasco0207@gmail.com>
2026-09-07 08:30:38 +0100

    added tests, made major auth changes, few integration fixes

--- src/pages/Dashboard/organization/OrganizationDashboard.tsx
+++ src/pages/Dashboard/organization/OrganizationDashboard.tsx
@@ -1,21 +1,32 @@
 import DashboardTopNav from "@/components/navbars/DashboardTopNav"
 import OrgSidenav from "./components/OrgSidenav"
-import OrgProfile from "./pages/OrgProfile"
+import { Outlet } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 
 const OrganizationDashboard = () => {
+  const [mobileNavOpen, setMobileNavOpen] = useState(false)
+  useEffect(() => {
+    if (!mobileNavOpen) return
+    const closeOnEscape = (event: KeyboardEvent) => {
+      if (event.key === "Escape") setMobileNavOpen(false)
+    }
+    window.addEventListener("keydown", closeOnEscape)
+    return () => window.removeEventListener("keydown", closeOnEscape)
+  }, [mobileNavOpen])
   return (
     <main className='flex flex-row w-full '>
       <section className='hidden md:block w-[256px]'>
         <OrgSidenav />
       </section>
 
-      <section className='md:w-[calc(100%-256px)] w-full'>
-        <DashboardTopNav />
+      <section inert={mobileNavOpen ? true : undefined} className='md:w-[calc(100%-256px)] w-full'>
+        <DashboardTopNav onMenuToggle={() => setMobileNavOpen(open => !open)} />
+        <OrgSidenav isOpen={mobileNavOpen} onClose={() => setMobileNavOpen(false)} mobileOnly />
 
         {/* Pages */}
         <div className='md:px-4'>
-          <OrgProfile />
+          <Outlet />
         </div>
 
       </section>
